19f34b19662d94a8612164a250e806883831894c
StanislawMarszalek <stanislawmarszalek890@gmail.com>
2026-04-22 16:11:43 +0200

    Add docstrings

--- Python/functions.py
+++ Python/functions.py
@@ -2,6 +2,12 @@ import random
 from math import sqrt
 
 def print_matrix(graph:list[list[int]])->None:
+    """
+    Print an adjacency matrix as a table
+    
+    :param graph: Adjacency matrix
+    :type graph: list[list[int]]
+    """
     graph_size:int=len(graph)
     print("  |", * [i + 1 for i in range(graph_size)])
     print("--+", "-" * 2 * len(graph), sep="")
@@ -11,6 +17,12 @@ def print_matrix(graph:list[list[int]])->None:
     return
 
 def empty_graph()->list[list[int]]:
+    """
+    Ask for number of nodes and create an empty matrix of size number_of_nodes X number_of_nodes
+    
+    :return: Adjacency matrix of the nodes
+    :rtype: list[list[int]]
+    """
     try:
         number_of_nodes=abs(int(input('Enter number of nodes: ')))
         matrix=[[0]*number_of_nodes for _ in range(number_of_nodes)]
@@ -20,6 +32,13 @@ def empty_graph()->list[list[int]]:
         return [[-1]]
 
 def full_graph()->list[list[int]]:
+    """
+    Ask for number of nodes and max distance between them, 
+    then create a full matrix of size number_of_nodes X number_of_nodes 
+    
+    :return: Adjacency matrix of the nodes with distances between them
+    :rtype: list[list[int]]
+    """
     try:
         number_of_nodes:int=abs(int(input('Enter number of nodes: ')))
         max_lenght:int=abs(int(input("Enter max length: ")))
@@ -37,6 +56,14 @@ def full_graph()->list[list[int]]:
         return [[-1]]
 
 def create_distance_graph(cities:list[list[int]])->list[list[float]]:
+    """
+    Creates an adjacency matrix of the cities with distances between them
+    
+    :param cities: Matrix of the cities with their coordinates
+    :type cities: list[list[int]]
+    :return: Adjacency matrix of the cities with distances between them
+    :rtype: list[list[float]]
+    """
     number_of_cities:int = len(cities)
     graph:list[list[float]]= [[0.0] * number_of_cities for _ in range(number_of_cities)]
     for i in range(number_of_cities):
